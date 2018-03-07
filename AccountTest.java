e48bf38d8d0beecd09b6ca19fc2dd0dc2955e139
jonatasfonsec <jonatas.fonsec@gmail.com>
2018-03-07 20:27:01 -0300

    Update AccountTest.java
    Alterado o AccountTest.java para criar duas instâncias do tipo Account e definir seus parametros.

--- AccountTest.java
+++ AccountTest.java
@@ -1,28 +1,17 @@
-// Fig. 3.2: AccountTest.java
-// Creating and manipulating an Account object.
-import java.util.Scanner;
+// Fig. 3.6: AccountTest.java
+// Using the Account constructor to initialize the name instance
+// variable at the time each Account object is created.
 
 public class AccountTest
 {
-  public static void main(String[] args)
-  {
-    //create a Scanner object to obtain input from the command window
-    Scanner input = new Scanner(System.in);
-    
-    //create an Account object and assign it to myAccount
-    Account myAccount = new Account();
-    
-    //display initial value of name (null)
-    System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-    
-    //prompt for and read name
-    System.out.println("Please enter the name:");
-    String theName = input.nextLine(); //read a line of text
-    myAccount.setName(theName); // put theName in myAccount
-    System.out.println(); // outputs a blank line
-    
-    //display the name stored in object myAccount
-    System.out.printf("Name in object myAccount is: %n%s%n" ,
-      myAccount.getName());
-  }
-}// end class AccountTest
+ public static void main(String[] args)
+ {
+   // create two Account objects
+ Account account1 = new Account("Jane Green");
+ Account account2 = new Account("John Blue");
+
+   // display initial value of name for each Account
+ System.out.printf("account1 name is: %s%n", account1.getName());
+ System.out.printf("account2 name is: %s%n", account2.getName());
+ }
+} // end class AccountTest
